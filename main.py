b7c27b024d83f5e34044dfc3e88d136da8539081
Maddison Hobbs <maddimay@amazon.com>
2025-05-27 11:49:11 +0100

    Create Login and Register pages

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.secret_key = 'your_secret'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password1')
+    # TODO: Implement login validation and logic
+
+    return render_template('auth/login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    # TODO: Implement register validation and logic
+
+    return render_template('auth/register.html')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
